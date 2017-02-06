0b0b03417a1efc4d7a9120333ee58ddbec7fc19b
135yshr <isago@praisefirst.jp>
2017-02-06 11:53:42 +0900

    add get player property

--- world/Plugins/Holocraft/holoworld.lua
+++ world/Plugins/Holocraft/holoworld.lua
@@ -4,6 +4,7 @@ function Initialize(Plugin)
 
     -- Hooks
     cPluginManager:AddHook(cPluginManager.HOOK_CHUNK_GENERATING, OnChunkGenerating)
+    cPluginManager:AddHook(cPluginManager.HOOK_PLAYER_JOINED, PlayerJoined);
 
     LOG("Initialised " .. Plugin:GetName() .. " v." .. Plugin:GetVersion())
 
@@ -15,4 +16,10 @@ function OnChunkGenerating(a_World, a_ChunkX, a_ChunkZ, a_ChunkDesc)
     a_ChunkDesc:SetUseDefaultHeight(false)
     a_ChunkDesc:SetUseDefaultComposition(false)
     a_ChunkDesc:SetUseDefaultFinish(false)
+end
+
+function PlayerJoined(Player)
+    -- enable flying
+    Player:SetCanFly(true)
+    LOG(Player:GetName() .. " joined")
 end
